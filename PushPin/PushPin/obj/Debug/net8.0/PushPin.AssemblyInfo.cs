508982677a0056389aa47cdfbed9d343614c2468
Zujin8010 <lxtot8010@gmail.com>
2025-04-06 03:00:04 +0800

    2504_IgnoreAdd

--- PushPin/PushPin/obj/Debug/net8.0/PushPin.AssemblyInfo.cs
+++ PushPin/PushPin/obj/Debug/net8.0/PushPin.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PushPin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ad24928c166f94d5c64158e3cde1bbc56cac5c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+def9e89104dd4e680f1b7739269ab6ba47cf085b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PushPin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PushPin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
