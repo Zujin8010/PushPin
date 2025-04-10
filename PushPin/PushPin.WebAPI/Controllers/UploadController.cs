961803b2c0dd76dd0a2d07881b4f3951856c9e1d
Zujin <lxtot8010@gmail.com>
2025-04-10 22:07:08 +0800

    feat/new upload controller

---
+++ PushPin/PushPin.WebAPI/Controllers/UploadController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PushPin.WebAPI.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
